918c5902c0f337aa4a6f835bf7de944e51660a89
Serhii_Stepanov <stepa.sergey.nov@gmail.com>
2023-06-29 14:57:13 +0300

    changed query #2

--- SQLite/query_2.sql
+++ SQLite/query_2.sql
@@ -1,10 +1,10 @@
--- -- Find the student with the highest grade point average in a particular subject
+-- Find the student with the highest grade point average in a particular subject
 
 SELECT subj.name AS subject, s.fullname AS student, ROUND(AVG(g.grade), 2) AS average_grade
 FROM grades AS g
 JOIN students AS s ON s.id = g.student_id
 JOIN subjects AS subj ON subj.id = g.subject_id
-WHERE subj.id = 1 -- Here 1 means a particular subject
+WHERE subj.id = 1 -- Here 1 means a particular subject; you can change it
 GROUP BY s.id
 ORDER BY average_grade DESC
 LIMIT 1;
