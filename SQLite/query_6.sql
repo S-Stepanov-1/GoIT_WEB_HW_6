7eeba9a2ace43b6a7f1240c85bba0596acbb9b5d
Serhii_Stepanov <stepa.sergey.nov@gmail.com>
2023-06-29 17:27:47 +0300

    changed query 6

--- SQLite/query_6.sql
+++ SQLite/query_6.sql
@@ -1,6 +1,7 @@
 -- Find a list of students in a particular group
+
 SELECT g.name AS [GROUP], s.fullname as student
 	FROM groups g
 	JOIN students s ON s.group_id = g.id
-    WHERE g.id = 1  --
+    WHERE g.id = 1  -- change "g.id" to get data for different groups
 	ORDER BY g.id;
